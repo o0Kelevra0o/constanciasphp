23ccb2d18aca84de38665ae85f7c081e84fe4d67
Julio Work <julio.gtsams@gmail.com>
2024-02-21 14:21:00 -0600

    Casi terminado

--- constancias.php
+++ constancias.php
@@ -1,39 +1,59 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Formulario de Constancias Médicas</title>
-<link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Constancias Médicas</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 </head>
+
 <body>
 
-<div>
-    <label id="fecha_hora"></label><br><br>
-    <label for="unidad">Unidad:</label>
-    <select id="unidad"></select><br><br>
-    <button id="generar_folio_btn">Generar Folio</button><br><br>
-    <label for="folio">Folio:</label>
-    <label id="folio_label"></label><br><br>
-    Datos del interesado:<br>
-    <input type="text" id="ape_pat" placeholder="Apellido Paterno"><br>
-    <input type="text" id="ape_mat" placeholder="Apellido Materno"><br>
-    <input type="text" id="nombres" placeholder="Nombre(s)"><br>
-    <input type="text" id="expediente" placeholder="Num. Expediente"><br><br>
-    Tiempo en clínica:<br>
-    <label for="hora_llegada">Hora de llegada:</label>
-    <input type="time" id="hora_llegada"><br>
-    <label for="hora_salida">Hora de salida:</label>
-    <input type="time" id="hora_salida"><br><br>
-    Datos de quien elaboró:<br>
-    <input type="text" id="elaboro" placeholder="Nombre y apellido de quien elaboró"><br><br>
-    <input type="checkbox" id="checkbox">
-    <label for="checkbox">He leído con atención los datos ingresados de la constancia y comprendo que es mi responsabilidad capturar correctamente</label><br><br>
-    <button id="guardar_btn" disabled>Guardar</button>
-    <button id="imprimir_btn" style="display:none;">Imprimir</button>
-</div>
+    <section class="formulario">
+        <div class="tiempo"><label id="fecha_hora"></label><br><br>
+        </div>
+        <div class="campo"><label for="unidad">Unidad:</label>
+            <select id="unidad"></select><br><br>
+        </div>
+        <div class="btnfolio"><button id="generar_folio_btn">Generar Folio</button><br><br>
+            <label for="folio">Folio:</label>
+            <label id="folio_label"></label><br><br>
+        </div>
+        <div class="usuario">
+            <div class="titulos">Datos del interesado:<br></div>
+            <input type="text" id="ape_pat" placeholder="Apellido Paterno"><br>
+            <input type="text" id="ape_mat" placeholder="Apellido Materno"><br>
+            <input type="text" id="nombres" placeholder="Nombre(s)"><br>
+            <input type="text" id="expediente" placeholder="Num. Expediente"><br><br>
+        </div>
+        <div class="tclinica">
+            <div class="titulos">Tiempo en clínica:</div>
+            <div class="horas">
+                <label for="hora_llegada">Hora de llegada:</label>
+                <input type="time" id="hora_llegada">
+                <label for="hora_salida">Hora de salida:</label>
+                <input type="time" id="hora_salida">
+            </div>
+        </div>
+        <div class="elaboro">
+            <div class="titulos">Datos de quien elaboró:</div>
+            <input type="text" id="elaboro" placeholder="Nombre completo de quien elaboró"><br><br>
+        </div>
+        <div class="check">
+            <input type="checkbox" id="checkbox">
+            <label for="checkbox">He leído con atención los datos ingresados de la constancia y comprendo que es mi
+                responsabilidad capturar correctamente</label><br><br>
+        </div>
+        <div class="btnguardar">
+            <button id="guardar_btn" disabled>Guardar</button>
+            <button id="imprimir_btn" style="display:none;">Imprimir</button>
+        </div>
 
-<script src="scripts.js"></script>
+        <div class="imagen"><img src="/img/base.jpg" alt=""></div>
+    </section>
+    <script src="scripts.js"></script>
 </body>
-</html>
 
+</html>
--- scripts.js
+++ scripts.js
@@ -1,166 +1,240 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtener fecha y hora actual
-    var fechaHora = new Date().toLocaleString('es-MX', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'});
-    document.getElementById("fecha_hora").textContent = fechaHora;
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener fecha y hora actual
+  var fechaHora = new Date().toLocaleString("es-MX", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  document.getElementById("fecha_hora").textContent = fechaHora;
 
-    // Obtener unidades desde la base de datos
+  // Obtener unidades desde la base de datos
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        console.log(this.responseText); // Agrega esta línea para depurar la respuesta
+        var unidades = JSON.parse(this.responseText);
+        var selectUnidades = document.getElementById("unidad");
+        unidades.forEach(function (unidad) {
+          var option = document.createElement("option");
+          option.value = unidad.ID;
+          option.textContent = unidad.nombre;
+          selectUnidades.appendChild(option);
+        });
+      } else {
+        console.error("Error en la solicitud: " + this.status);
+      }
+    }
+  };
+  xhr.open("GET", "obtener_unidades.php", true);
+  xhr.send();
+
+  // Generar folio
+  document
+    .getElementById("generar_folio_btn")
+    .addEventListener("click", function () {
+      var unidad = document.getElementById("unidad").value;
+      var d = new Date();
+      var year = d.getFullYear().toString().substr(-2);
+
+      // Obtener la tabla correspondiente
+      var tabla;
+      switch (unidad) {
+        case "CU":
+          tabla = "Constancias_Cuauhtemoc";
+          break;
+        case "TA":
+          tabla = "Constancias_Tasquena";
+          break;
+        case "TI":
+          tabla = "Constancias_Ticoman";
+          break;
+        case "ZA":
+          tabla = "Constancias_Zaragoza";
+          break;
+        case "SU":
+          tabla = "Constancias_Salauno";
+          break;
+        case "TH":
+          tabla = "Constancias_Theraclinic";
+          break;
+        case "OL":
+          tabla = "Constancias_Olab";
+          break;
+        case "AZ":
+          tabla = "Constancias_Azura";
+          break;
+        default:
+          console.error("Clínica no válida");
+          return; // Salir de la función si la clínica no es válida
+      }
+
+      // Obtener el consecutivo actualizado
+      var xhrConsecutivo = new XMLHttpRequest();
+      xhrConsecutivo.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          var ultimoConsecutivo = parseInt(this.responseText);
+          if (!isNaN(ultimoConsecutivo)) {
+            // Generar folio con el nuevo consecutivo
+            var consecutivo = ("0000" + (ultimoConsecutivo + 1)).slice(-4);
+            var folio = unidad + year + consecutivo; // Ajustar el formato del folio
+
+            // Mostrar el folio generado
+            document.getElementById("folio_label").textContent = folio;
+          } else {
+            console.error("Error al obtener el último consecutivo");
+          }
+        }
+      };
+      xhrConsecutivo.open(
+        "GET",
+        "conexion.php?accion=obtener_ultimo_consecutivo&tabla=" + tabla,
+        true
+      );
+      xhrConsecutivo.send();
+    });
+
+  // Habilitar botón guardar al marcar el checkbox
+  document.getElementById("checkbox").addEventListener("change", function () {
+    document.getElementById("guardar_btn").disabled = !this.checked;
+  });
+
+  // Función para guardar datos
+  document.getElementById("guardar_btn").addEventListener("click", function () {
+    var unidadSeleccionada = document.getElementById("unidad").value;
+    var folio = document.getElementById("folio_label").textContent;
+    var ape_pat = document.getElementById("ape_pat").value;
+    var ape_mat = document.getElementById("ape_mat").value;
+    var nombres = document.getElementById("nombres").value;
+    var expediente = document.getElementById("expediente").value;
+    var hora_llegada = document.getElementById("hora_llegada").value;
+    var hora_salida = document.getElementById("hora_salida").value;
+    var elaboro = document.getElementById("elaboro").value;
+
+    // Determinar la tabla correspondiente
+    var tabla;
+    switch (unidadSeleccionada) {
+      case "CU":
+        tabla = "Constancias_Cuauhtemoc";
+        break;
+      case "TA":
+        tabla = "Constancias_Tasquena";
+        break;
+      case "TI":
+        tabla = "Constancias_Ticoman";
+        break;
+      case "ZA":
+        tabla = "Constancias_Zaragoza";
+        break;
+      case "SU":
+        tabla = "Constancias_Salauno";
+        break;
+      case "TH":
+        tabla = "Constancias_Theraclinic";
+        break;
+      case "OL":
+        tabla = "Constancias_Olab";
+        break;
+      case "AZ":
+        tabla = "Constancias_Azura";
+        break;
+      default:
+        console.error("Clínica no válida");
+        return; // Salir de la función si la clínica no es válida
+    }
+
+    // Construir la consulta SQL
+    var sql =
+      "INSERT INTO " +
+      tabla +
+      " (Folio, Ape_pat, Ape_Mat, Nombres, Expediente, hora_llegada, hora_salida, Elaboro) " +
+      "VALUES ('" +
+      folio +
+      "', '" +
+      ape_pat +
+      "', '" +
+      ape_mat +
+      "', '" +
+      nombres +
+      "', '" +
+      expediente +
+      "', '" +
+      hora_llegada +
+      "', '" +
+      hora_salida +
+      "', '" +
+      elaboro +
+      "')";
+
+    // Realizar la solicitud AJAX para ejecutar la consulta SQL
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (this.readyState === 4) {
-            if (this.status === 200) {
-                console.log(this.responseText); // Agrega esta línea para depurar la respuesta
-                var unidades = JSON.parse(this.responseText);
-                var selectUnidades = document.getElementById("unidad");
-                unidades.forEach(function(unidad) {
-                    var option = document.createElement("option");
-                    option.value = unidad.ID;
-                    option.textContent = unidad.nombre;
-                    selectUnidades.appendChild(option);
-                });
-            } else {
-                console.error("Error en la solicitud: " + this.status);
-            }
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          console.log("Datos guardados exitosamente en " + tabla);
+          // Mostrar mensaje de confirmación
+          alert("Los datos se guardaron correctamente.");
+          // Mostrar el botón de imprimir después de guardar correctamente
+          document.getElementById("imprimir_btn").style.display = "inline";
+        } else {
+          console.error("Error al guardar datos: " + this.responseText);
+          // Mostrar mensaje de error
+          alert(
+            "Ocurrió un error al guardar los datos. Por favor, intenta nuevamente."
+          );
         }
+      }
     };
-    xhr.open("GET", "obtener_unidades.php", true);
-    xhr.send();
+    xhr.open("POST", "conexion.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("sql=" + encodeURIComponent(sql));
+  });
 
-    // Generar folio
-    document.getElementById("generar_folio_btn").addEventListener("click", function() {
-        var unidad = document.getElementById("unidad").value;
-        var d = new Date();
-        var year = d.getFullYear().toString().substr(-2);
-        
-        // Obtener la tabla correspondiente
-        var tabla;
-        switch (unidad) {
-            case "CU":
-                tabla = "Constancias_Cuauhtemoc";
-                break;
-            case "TA":
-                tabla = "Constancias_Tasquena";
-                break;
-            case "TI":
-                tabla = "Constancias_Ticoman";
-                break;
-            case "ZA":
-                tabla = "Constancias_Zaragoza";
-                break;
-            case "SU":
-                tabla = "Constancias_Salauno";
-                break;
-            case "TH":
-                tabla = "Constancias_Theraclinic";
-                break;
-            case "OL":
-                tabla = "Constancias_Olab";
-                break;
-            case "AZ":
-                tabla = "Constancias_Azura";
-                break;
-            default:
-                console.error("Clínica no válida");
-                return; // Salir de la función si la clínica no es válida
-        }
+  // Agregar evento de clic al botón "Imprimir"
+  document
+    .getElementById("imprimir_btn")
+    .addEventListener("click", function () {
+      // Obtener los datos del formulario
+      const folio = document.getElementById("folio_label").textContent;
+      const fecha = new Date().toLocaleDateString("es-MX");
+      const nombreCompleto =
+        document.getElementById("ape_pat").value +
+        " " +
+        document.getElementById("ape_mat").value +
+        " " +
+        document.getElementById("nombres").value;
+      const expediente = document.getElementById("expediente").value;
+      const unidad =
+        document.getElementById("unidad").options[
+          document.getElementById("unidad").selectedIndex
+        ].text;
+      const horaLlegada = document.getElementById("hora_llegada").value;
+      const horaSalida = document.getElementById("hora_salida").value;
+      const elaboro = document.getElementById("elaboro").value;
 
-        // Obtener el consecutivo actualizado
-        var xhrConsecutivo = new XMLHttpRequest();
-        xhrConsecutivo.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var ultimoConsecutivo = parseInt(this.responseText);
-                if (!isNaN(ultimoConsecutivo)) {
-                    // Generar folio con el nuevo consecutivo
-                    var consecutivo = ("0000" + (ultimoConsecutivo + 1)).slice(-4);
-                    var folio = unidad + year + consecutivo; // Ajustar el formato del folio
+      // Creamos una nueva instancia de jsPDF
+      const doc = new jsPDF();
 
-                    // Mostrar el folio generado
-                    document.getElementById("folio_label").textContent = folio;
-                } else {
-                    console.error("Error al obtener el último consecutivo");
-                }
-            }
-        };
-        xhrConsecutivo.open("GET", "conexion.php?accion=obtener_ultimo_consecutivo&tabla=" + tabla, true);
-        xhrConsecutivo.send();
+      // Definimos el fondo
+      doc.addImage("/img/fondo.jpg", "JPEG", 0, 0, 210, 297); // Ajustar tamaño según necesidades
+
+      // Agregamos los datos al PDF
+      doc.setFontSize(12);
+      doc.text(`Folio: ${folio}`, 20, 80);
+      doc.text(`Fecha: ${fecha}`, 20, 90);
+      doc.text("A quien corresponda:", 20, 100);
+      doc.text(
+        `Por medio de la presente, se hace constar que el (la) C. ${nombreCompleto}, con No. de Expediente: ${expediente}, asistió el día de hoy a atención médica en la ${unidad}, entre las ${horaLlegada} y las ${horaSalida}.`,
+        20,
+        110
+      );
+      doc.text(`${elaboro}`, 20, 150); // Ajustar posición según necesidades
+
+      // Mostramos el documento PDF en una nueva ventana
+      const pdfDataUri = doc.output("datauristring");
+      window.open(pdfDataUri, "_blank");
     });
-
-    // Habilitar botón guardar al marcar el checkbox
-    document.getElementById("checkbox").addEventListener("change", function() {
-        document.getElementById("guardar_btn").disabled = !this.checked;
-    });
-
-    // Función para guardar datos
-    document.getElementById("guardar_btn").addEventListener("click", function() {
-        var unidadSeleccionada = document.getElementById("unidad").value;
-        var folio = document.getElementById("folio_label").textContent;
-        var ape_pat = document.getElementById("ape_pat").value;
-        var ape_mat = document.getElementById("ape_mat").value;
-        var nombres = document.getElementById("nombres").value;
-        var expediente = document.getElementById("expediente").value;
-        var hora_llegada = document.getElementById("hora_llegada").value;
-        var hora_salida = document.getElementById("hora_salida").value;
-        var elaboro = document.getElementById("elaboro").value;
-    
-        // Determinar la tabla correspondiente
-        var tabla;
-        switch (unidadSeleccionada) {
-            case "CU":
-                tabla = "Constancias_Cuauhtemoc";
-                break;
-            case "TA":
-                tabla = "Constancias_Tasquena";
-                break;
-            case "TI":
-                tabla = "Constancias_Ticoman";
-                break;
-            case "ZA":
-                tabla = "Constancias_Zaragoza";
-                break;
-            case "SU":
-                tabla = "Constancias_Salauno";
-                break;
-            case "TH":
-                tabla = "Constancias_Theraclinic";
-                break;
-            case "OL":
-                tabla = "Constancias_Olab";
-                break;
-            case "AZ":
-                tabla = "Constancias_Azura";
-                break;
-            default:
-                console.error("Clínica no válida");
-                return; // Salir de la función si la clínica no es válida
-        }
-    
-        // Construir la consulta SQL
-        var sql = "INSERT INTO " + tabla + " (Folio, Ape_pat, Ape_Mat, Nombres, Expediente, hora_llegada, hora_salida, Elaboro) " +
-                  "VALUES ('" + folio + "', '" + ape_pat + "', '" + ape_mat + "', '" + nombres + "', '" + expediente + "', '" +
-                  hora_llegada + "', '" + hora_salida + "', '" + elaboro + "')";
-    
-        // Realizar la solicitud AJAX para ejecutar la consulta SQL
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    console.log("Datos guardados exitosamente en " + tabla);
-                    // Mostrar mensaje de confirmación
-                    alert("Los datos se guardaron correctamente.");
-                    // Mostrar el botón de imprimir después de guardar correctamente
-                    document.getElementById("imprimir_btn").style.display = "inline";
-                } else {
-                    console.error("Error al guardar datos: " + this.responseText);
-                    // Mostrar mensaje de error
-                    alert("Ocurrió un error al guardar los datos. Por favor, intenta nuevamente.");
-                }
-            }
-        };
-        xhr.open("POST", "conexion.php", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send("sql=" + encodeURIComponent(sql));
-    });    
 });
-
-
-
-
